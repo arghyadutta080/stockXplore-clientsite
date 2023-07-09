5645b88c9c00b3eaca1872e65a08bebdec0e799f
Arghya <arghyadutta080@gmail.com>
2023-07-10 04:26:32 +0530

    main page user experience improved by adding more public api related to stock

--- src/contexts/StockState.js
+++ src/contexts/StockState.js
@@ -8,22 +8,24 @@ const StockState = (props) => {
     const [user , setUser]=useState(false);
     const [userInfo , setUserInfo] = useState([]);
     const [stockInfo, setStockInfo] = useState(StockData);
+    const [stockName , setStockName] = useState('stock symbol');
+    const [currentStockPrice , setCurrentStockPrice] = useState(0);
+    const [currency , setCurrency] = useState('');
+    const [companyName , setCompanyName] = useState('company name');
+    const [stockLogo , setStockLogo] = useState('') ;
+
     const handleModal = () => {
       setModal(true) ;
     }
-    
-    
 
     const handleClose = (e)=>{
       if(e.target.id==='container' || e.target.id === 'close'){
-        setModal(false);
-      
-        
-      }
-      
+        setModal(false);       
+      }   
     }
+
   return (
-    <StockContext.Provider value={{modal , handleModal , user , setUser , stockInfo , setStockInfo , handleClose, userInfo, setUserInfo}}>
+    <StockContext.Provider value={{ modal, handleModal, user, setUser, stockInfo, setStockInfo, handleClose, userInfo, setUserInfo, stockName, setStockName, currentStockPrice, setCurrentStockPrice, currency, setCurrency, companyName, setCompanyName, stockLogo, setStockLogo }}>
         {props.children}
     </StockContext.Provider>
   )
